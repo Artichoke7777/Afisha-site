cac62b48e033da6ec08023d791cd98fe506e2ab9
Artichoke7777 <128820525+Artichoke7777@users.noreply.github.com>
2025-09-10 07:08:45 +0000

    Add files via upload

--- scrypt.js
+++ scrypt.js
@@ -6,17 +6,17 @@ thing.forEach((item) => {
     });
 });
 let calender = {
-    months : ["Январь", "Февраль", "Март", "Апрель"],
+    months : ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
     current : 0,
-    Next : function() {
+    Prev : function() {
         this.current++;
         if (this.current >= this.months.length){
             this.current = 0
         }
-            
+        return months[this.current]
     },
-    Prev : function() {
-        this.current--;
+    Next : function() {
+        this.current --;
         if (this.current < 0){
             this.current = this.months.length - 1
         }
@@ -33,6 +33,7 @@ leftarr.addEventListener('click', () => {
     months.textContent = calender.months[calender.current]
 })
 rightarr.addEventListener('click', () => {
-    months.textContent = calender.Prev()
+    calender.Prev()
+    months.textContent = calender.months[calender.current]
 })
 
